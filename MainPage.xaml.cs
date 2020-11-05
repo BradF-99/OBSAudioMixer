ea50ad71b1bdfb5e92216781e72785ee58f5d138
Brad Fuller <37586752+BradF-99@users.noreply.github.com>
2020-11-05 22:05:14 +1000

    Created basic auth page

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using OBSWebsocketDotNet;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -13,7 +15,7 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace OBSAudioMixer
 {
@@ -22,9 +24,53 @@ namespace OBSAudioMixer
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private OBSWebsocket _obs;
+
         public MainPage()
         {
             this.InitializeComponent();
+            _obs = new OBSWebsocket();
+
+            _obs.Connected += OnConnect;
+        }
+
+        private void btnLogin_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            tbIP.IsEnabled = false;
+            tbPort.IsEnabled = false;
+            tbPassword.IsEnabled = false;
+            btnLogin.IsEnabled = false;
+
+            if (!_obs.IsConnected)
+            {
+                try
+                {
+                    _obs.Connect($"ws://{tbIP.Text}:{tbPort.Text}", tbPassword.Text);
+                }
+                catch (Exception ex)
+                {
+                    if (ex is AuthFailureException)
+                    {
+                        FlyoutBase.ShowAttachedFlyout(tbPassword);
+                    }
+                    else
+                    {
+                        FlyoutBase.ShowAttachedFlyout(tbIP);
+                    }
+                }
+            }
+
+            _obs.Disconnect();
+
+            tbIP.IsEnabled = true;
+            tbPort.IsEnabled = true;
+            tbPassword.IsEnabled = true;
+            btnLogin.IsEnabled = true;
+        }
+
+        private void OnConnect(object sender, EventArgs e)
+        {
+            Frame.Navigate(typeof(MixerPage), _obs);
         }
     }
 }

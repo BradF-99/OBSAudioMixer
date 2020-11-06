21745dd176e351c4dd1b08779b565f5f68dd64b7
Brad Fuller <37586752+BradF-99@users.noreply.github.com>
2020-11-06 12:32:57 +1000

    Created source class to hold scene and slider items

---
+++ SourceClass.cs
@@ -0,0 +1,22 @@
+﻿using OBSWebsocketDotNet.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+
+namespace OBSAudioMixer
+{
+    class SourceClass
+    {
+        public SourceClass(SceneItem sceneItem, Slider slider)
+        {
+            this.sceneItem = sceneItem;
+            this.slider = slider;
+        }
+
+        public SceneItem sceneItem { get; set; }
+        public Slider slider { get; set; }
+    }
+}

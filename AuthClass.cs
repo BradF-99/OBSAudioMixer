779aa94aec2518518539e637572f8b3c13571820
Brad Fuller <37586752+BradF-99@users.noreply.github.com>
2020-11-06 12:31:18 +1000

    Added class to pass auth between pages

---
+++ AuthClass.cs
@@ -0,0 +1,16 @@
+﻿namespace OBSAudioMixer
+{
+    class AuthClass
+    {
+        public string IP;
+        public string Port;
+        public string Password;
+
+        public AuthClass(string ip, string port, string password)
+        {
+            IP = ip;
+            Port = port;
+            Password = password;
+        }
+    }
+}
